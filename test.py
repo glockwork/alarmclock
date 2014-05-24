d23da7e871357bd2a8e139d1bef5791859a47f7c
paddycr <pi@raspberrypi.(none)>
2014-05-24 16:06:38 +0000

    new loop

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import RPi.GPIO as GPIO
+import time
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
-##GPIO.output(7,True)
-GPIO.output(7,False)
+# simple while loop to turn the bell off and on
+
+
+var=1
+print "Start Loop"
+while var ==1 :
+    print "Set output to False"
+    GPIO.output(7,False)
+    time.sleep(1)
+    print "Set output to True"
+    GPIO.output(7,True)
+    time.sleep(1)
